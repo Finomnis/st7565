eb4730d0b1ff8706970cdd7219660fd3df94ecd6
Finomnis <finomnis@gmail.com>
2022-08-07 00:17:45 +0200

    Finish command serialization

--- src/command.rs
+++ src/command.rs
@@ -16,7 +16,7 @@ pub enum Command {
     DisplayStartLineSet { address: u8 },
     PageAddressSet { address: u8 },
     ColumnAddressSetUpper { address: u8 },
-    ColumnAddresSetLower { address: u8 },
+    ColumnAddressSetLower { address: u8 },
     AdcSelect { reverse: bool },
     DisplayNormalReverse { reverse: bool },
     DisplayAllPoints { on: bool },
@@ -52,7 +52,33 @@ where
             DisplayOnOff { on } => Single(0b10101110 | on as u8),
             DisplayStartLineSet { address } => Single(0b01000000 | (address & 0b00111111)),
             PageAddressSet { address } => Single(0b10110000 | (address & 0b00001111)),
-            _ => Single(0),
+            ColumnAddressSetUpper { address } => Single(0b00010000 | ((address >> 4) & 0b00001111)),
+            ColumnAddressSetLower { address } => Single(0b00000000 | (address & 0b00001111)),
+            AdcSelect { reverse } => Single(0b10100000 | reverse as u8),
+            DisplayNormalReverse { reverse } => Single(0b10100110 | reverse as u8),
+            DisplayAllPoints { on } => Single(0b10100100 | on as u8),
+            LcdBiasSet { bias_1_7 } => Single(0b10100010 | bias_1_7 as u8),
+            Reset => Single(0b11100010),
+            CommonOutputModeSelect { reverse_direction } => {
+                Single(0b11000000 | ((reverse_direction as u8) << 3))
+            }
+            PowerControlSet { operating_mode } => {
+                Single(0b00101000 | (operating_mode & 0b00000111))
+            }
+            V0VoltageRegulatorInternalResistorSet { resistor_ratio } => {
+                Single(0b00100000 | (resistor_ratio & 0b00000111))
+            }
+            ElectronicVolumeSet { volume_value } => Double(0b10000001, volume_value & 0b00111111),
+            StaticIndicatorSet { on, flash } => Double(0b10101100 | on as u8, flash as u8),
+            BoosterRatioSet { stepup_value } => Double(
+                0b11111000,
+                match stepup_value {
+                    BoosterRatio::StepUp_2x_3x_4x => 0b00000000,
+                    BoosterRatio::StepUp_5x => 0b00000001,
+                    BoosterRatio::StepUp_6x => 0b00000011,
+                },
+            ),
+            NOP => Single(0b11100011),
         };
 
         match data {
