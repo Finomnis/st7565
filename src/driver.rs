41976dbffad21d184e0b6f4c6a1d93ac03bfa341
Finomnis <finomnis@gmail.com>
2022-08-07 14:15:33 +0200

    Add LCD bias mode

--- src/driver.rs
+++ src/driver.rs
@@ -1,11 +1,15 @@
 use display_interface::WriteOnlyDataCommand;
 use embedded_hal::{blocking::delay::DelayMs, digital::v2::OutputPin};
 
-use crate::Error;
+use crate::{
+    command::{Command, SendSt7565Command},
+    Error,
+};
 
 /// ST7565 driver.
 pub struct ST7565<DI> {
     pub(crate) interface: DI,
+    pub(crate) lcd_bias_mode: bool,
 }
 
 impl<DI> ST7565<DI>
@@ -22,10 +26,27 @@ where
         RST: OutputPin<Error = PinE>,
         DELAY: DelayMs<u8>,
     {
-        rst.set_high().map_err(Error::Pin)?;
-        delay.delay_ms(1);
         rst.set_low().map_err(Error::Pin)?;
         delay.delay_ms(1);
-        rst.set_high().map_err(Error::Pin)
+        rst.set_high().map_err(Error::Pin)?;
+
+        // Initialize
+
+        // LCD Bias
+        self.interface.send_command(Command::LcdBiasSet {
+            bias_1_7: self.lcd_bias_mode,
+        })?;
+
+        // ADC Selection
+        // Common output mode selection
+
+        // v0 regulator resistor ratio
+        // electric volume
+
+        // power control
+
+        // initialize dram
+
+        Ok(())
     }
 }
--- src/driver_builder.rs
+++ src/driver_builder.rs
@@ -1,11 +1,11 @@
 use display_interface::WriteOnlyDataCommand;
 use embedded_hal::{blocking::delay::DelayMs, digital::v2::OutputPin};
 
-use crate::Error;
 use crate::ST7565;
 
 pub struct ST7565DriverBuilder<DI> {
     interface: DI,
+    lcd_bias_mode: bool,
 }
 
 impl<DI> ST7565DriverBuilder<DI>
@@ -13,12 +13,21 @@ where
     DI: WriteOnlyDataCommand,
 {
     pub fn new(interface: DI) -> Self {
-        Self { interface }
+        Self {
+            interface,
+            lcd_bias_mode: false,
+        }
+    }
+
+    pub fn lcd_bias(mut self, mode: bool) -> Self {
+        self.lcd_bias_mode = mode;
+        self
     }
 
     pub fn build(self) -> ST7565<DI> {
         ST7565 {
             interface: self.interface,
+            lcd_bias_mode: self.lcd_bias_mode,
         }
     }
 }
