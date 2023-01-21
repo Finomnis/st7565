a88705d4ebc1668ad5a857a898e3d1d9c195068b
Finomnis <finomnis@gmail.com>
2023-01-21 13:50:41 +0100

    Refactor page buffer struct

--- src/driver/page_buffer.rs
+++ src/driver/page_buffer.rs
@@ -1,24 +1,33 @@
+#[derive(Copy, Clone)]
+pub(crate) struct BufferPage<const WIDTH: usize> {
+    pub data: [u8; WIDTH],
+    pub dirty: Option<(usize, usize)>,
+}
+
 /// A graphics page buffer, required for graphics mode.
 ///
 /// Needs to be provided by the user.
-pub struct GraphicsPageBuffer<const WIDTH: usize, const PAGES: usize>(
-    pub [([u8; WIDTH], Option<(usize, usize)>); PAGES],
-);
+pub struct GraphicsPageBuffer<const WIDTH: usize, const PAGES: usize> {
+    pub(crate) pages: [BufferPage<WIDTH>; PAGES],
+}
 
 impl<const WIDTH: usize, const PAGES: usize> GraphicsPageBuffer<WIDTH, PAGES> {
     /// Creates a graphics page buffer.
     pub const fn new() -> Self {
-        Self(
+        Self {
             // Fill with full dirty flags to force an initial synchronization
-            [([0; WIDTH], Some((0, WIDTH))); PAGES],
-        )
+            pages: [BufferPage {
+                data: [0; WIDTH],
+                dirty: Some((0, WIDTH)),
+            }; PAGES],
+        }
     }
 
     /// Marks the entire buffer as dirty, causing a full retransmission of
-    /// all data at next `flush()`.
+    /// all data at next [`flush()`](crate::ST7565::flush).
     pub fn mark_dirty(&mut self) {
-        for page in &mut self.0 {
-            page.1 = Some((0, WIDTH));
+        for page in &mut self.pages {
+            page.dirty = Some((0, WIDTH));
         }
     }
 }
